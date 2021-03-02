8a52aede3c857bd317e2015836bdef2af2d60cf5
Vladimir <v1ztep@gmail.com>
2021-03-02 17:42:06 +0300

    Словарь по топ 15 языкам программирования

--- main.py
+++ main.py
@@ -11,13 +11,19 @@ def get_response(url, params=None):
 
 def main():
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
+    languages = ('JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'C#',
+                 'C', 'Go', 'Shell', 'Objective-C', 'Scala', 'Swift',
+                 'TypeScript')
     url = 'https://api.hh.ru/vacancies'
-    params = {'text': 'Python', 'area': '1', 'period': '30'}
 
-    response = get_response(url, params)
-    print(response.json())
+    vacancies_count = {}
+    for lang in languages:
+        params = {'text': f'Программист {lang}', 'area': '1', 'period': '30'}
+        response = get_response(url, params)
+        found_num = response.json()['found']
+        vacancies_count[lang] = found_num
 
+    print(vacancies_count)
 
 if __name__ == '__main__':
     main()
