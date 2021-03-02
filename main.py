cc98d9d26157e6bf65380aa439a5c8472e700e4a
Vladimir <v1ztep@gmail.com>
2021-03-02 17:08:50 +0300

    Вывел вакансии Москвы за последние 30 дней

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import requests
+
+import urllib3
+
+
+def get_response(url, params=None):
+    response = requests.get(url, params, verify=False)
+    response.raise_for_status()
+    return response
+
 
 def main():
-    pass
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    url = 'https://api.hh.ru/vacancies'
+    params = {'text': 'Python', 'area': '1', 'period': '30'}
+
+    response = get_response(url, params)
+    print(response.json())
 
 
 if __name__ == '__main__':
